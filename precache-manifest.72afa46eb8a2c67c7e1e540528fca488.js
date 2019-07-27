7c817551d91de7ced7195ed13c66cab5ff6d916d
Raghav Shiv <Raghav@Raghavs-MacBook-Air.local>
2019-07-26 19:34:22 -0700

    Updates

---
+++ precache-manifest.72afa46eb8a2c67c7e1e540528fca488.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c39bd30a1988a43b1b23d5a4d913e964",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "28fc8bde7c045134a407",
+    "url": "/clicky-game/static/css/main.fb8afd51.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/clicky-game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "28fc8bde7c045134a407",
+    "url": "/clicky-game/static/js/main.c1156eb1.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
